5dfce834ce528b9f7d8f5cb290685a4ab31da1cf
jobayercse71 <jobayercse71.com>
2022-09-23 22:51:25 +0600

    error handle middleware added

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const router = require('./routes/v1/user.route');
+const errorHandler = require('./middleware/errorHandler');
+const userRouter = require('./routes/v1/user.route');
 const app = express();
 const port = process.env.port || 4000;
 
-app.use('/users',router)
+app.use('/users',userRouter)
 
 
 app.all('*',(req,res)=>{
     res.send('No route found')
 })
+
+
+
+// global error handler
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
